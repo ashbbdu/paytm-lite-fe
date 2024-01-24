85624164d2002ccd7c1e36b164b498245fcb8003
Ashish Srivastava <ashishsrivastava7007@gmail.com>
2024-01-25 01:27:35 +0530

    done

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -2,7 +2,6 @@ import React, { memo } from 'react'
 import { useSelector } from "react-redux"
 const UserDetails = memo(() => {
 const { bankBalance } = useSelector(state => state.bank)
-console.log(bankBalance , "bankbal");
   return (
     <div>
         Your current balance is : $ {bankBalance}
